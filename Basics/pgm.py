bcc0432a3829268611f10a4ec8c43997beb88479
VismayaM-2003 <128399904+VismayaM-2003@users.noreply.github.com>
2023-10-20 17:46:40 +0530

    Delete Basics directory

--- Basics/pgm.py
+++
@@ -1,6 +0,0 @@
-
-import os
-if os.path.exists("myfile.txt"):
-    os.remove("myfile.txt")
-else:
-    print("The file does not exists")
--- Basics/python.py
+++
@@ -1,4 +0,0 @@
-
-print("Hello World!!!")
-print("Welcome to python")
-print("You  can learn  python here")
